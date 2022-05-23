d3de456c196c74253a069f6b77594f0fd933f380
Piootrekk <piotr.wilkowiecki100@gmail.com>
2022-05-23 15:14:07 +0200

    AudioMixery

--- Assets/Scripts/MenuScripts/MenuSettings.cs
+++ Assets/Scripts/MenuScripts/MenuSettings.cs
@@ -2,20 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.Audio;
 
 public class MenuSettings : MonoBehaviour
 {
     
     public TMP_Dropdown resolutionDropdown;
-    public TMP_Dropdown qualityDropdown;
 
+    public AudioMixer MasterAudio;
 
     Resolution[] resolutions;
 
     private void Start()
     {
-        qualityDropdown.value = QualitySettings.GetQualityLevel();
-        qualityDropdown.RefreshShownValue();
+
         resolutions = Screen.resolutions;
         resolutionDropdown.ClearOptions();
         List<string> options = new List<string>();
@@ -23,10 +23,11 @@ public class MenuSettings : MonoBehaviour
         int currentResolutionIndex = 0;
         for (int i = 0; i < resolutions.Length; i++)
         {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
+            string option = resolutions[i].width + " x " + resolutions[i].height + " @ " + resolutions[i].refreshRate + "Hz";
             options.Add(option);
 
-            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height)
+            if (resolutions[i].width == Screen.currentResolution.width && resolutions[i].height == Screen.currentResolution.height
+                && resolutions[i].refreshRate == Screen.currentResolution.refreshRate)
             {
                 currentResolutionIndex = i;
             }
@@ -55,4 +56,19 @@ public class MenuSettings : MonoBehaviour
         Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 
+
+    public void SetMasterVolume(float volume)
+    {
+        MasterAudio.SetFloat("MasterMixer", volume);
+    }
+
+    public void SetEffectsVolume(float volume)
+    {
+        MasterAudio.SetFloat("EffectsMixer", volume);
+    }
+
+    public void SetMusicVolume(float volume)
+    {
+        MasterAudio.SetFloat("MusicMixer", volume);
+    }
 }
